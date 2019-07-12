5ebc1c4f044267e1b11282ce6e10b57f350624fa
Michael Haynes <mhaynes@nexient.com>
2019-07-12 10:14:19 -0400

    Started a .Net Core Hello World Project

---
+++ HelloWorldNetCore/HelloWorldNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

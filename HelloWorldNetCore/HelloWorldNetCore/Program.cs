5c5ce6889fda78ce9774de59d17c9fa26960cffa
Michael Haynes <mhaynes@nexient.com>
2019-07-15 11:33:02 -0400

    Testing

--- HelloWorldNetCore/HelloWorldNetCore/Program.cs
+++ HelloWorldNetCore/HelloWorldNetCore/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorldNetCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
